ee1d7d3546dcfe88858875047c35e9c11a121e69
mynus <jjw.matheussandri@gmail.com>
2019-08-04 01:41:06 -0300

    Criando operacao de instanciacao

--- src/main/java/com/jjw/cursomc/CursomcApplication.java
+++ src/main/java/com/jjw/cursomc/CursomcApplication.java
@@ -1,13 +1,28 @@
 package com.jjw.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.jjw.cursomc.domain.Categoria;
+import com.jjw.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		repository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
