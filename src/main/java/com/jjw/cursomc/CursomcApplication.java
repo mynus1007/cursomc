939e2dc0c44ef679d4fe98bc7ea6694ee9698dd6
mynus <jjw.matheussandri@gmail.com>
2019-08-04 17:35:44 -0300

    Cidade e estado criados e instanciados com Repositories e associacao de um pra muitos e muitos pra um

--- src/main/java/com/jjw/cursomc/CursomcApplication.java
+++ src/main/java/com/jjw/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.jjw.cursomc.domain.Categoria;
+import com.jjw.cursomc.domain.Cidade;
+import com.jjw.cursomc.domain.Estado;
 import com.jjw.cursomc.domain.Produto;
 import com.jjw.cursomc.repositories.CategoriaRepository;
+import com.jjw.cursomc.repositories.CidadeRepository;
+import com.jjw.cursomc.repositories.EstadoRepository;
 import com.jjw.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
 	CategoriaRepository categoriaRepository;
 	@Autowired
 	ProdutoRepository produtoRepository;
+	@Autowired
+	EstadoRepository estadoRepository;
+	@Autowired
+	CidadeRepository cidadeRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -38,7 +46,23 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 }
